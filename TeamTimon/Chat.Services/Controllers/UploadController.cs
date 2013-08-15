36aa31de24acd4bb19dd7d39ea912b4f2d8362fd
Anton <ant.dobrev@gmail.com>
2013-08-15 05:45:03 -0700

    UploadController added. Working Upload service.
    Please use the script.js to consume the service with the client.

---
+++ TeamTimon/Chat.Services/Controllers/UploadController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Chat.Services.Utils;
+
+namespace Chat.Services.Controllers
+{
+    public class UploadController : ApiController
+    {
+        [HttpPost]
+        [ActionName("upload-file")]
+        public async Task<HttpResponseMessage> Post()
+        {
+            string folderName = @"Uploads";
+            string PATH = HttpContext.Current.Server.MapPath("~/" + folderName);
+            string rootUrl = Request.RequestUri.AbsoluteUri.Replace(Request.RequestUri.AbsolutePath, String.Empty);
+
+            if (Request.Content.IsMimeMultipartContent())
+            {
+                var streamProvider = new MultipartFormDataStreamProvider(PATH);
+                //var task = Request.Content.ReadAsMultipartAsync(streamProvider).ContinueWith<IEnumerable<FileDesc>>(t =>
+                //{
+
+                //    //if (t.IsFaulted || t.IsCanceled)
+                //    //{
+                //    //    throw new HttpResponseException(HttpStatusCode.BadGateway);
+                //    //}
+
+                //    var fileInfo = streamProvider.FileData.Select(i =>
+                //    {
+                //        var info = new FileInfo(i.LocalFileName);
+                //        return new FileDesc(info.Name, rootUrl+"/"+folderName+"/"+info.Name, info.Length);
+                //    });
+                //    return fileInfo;
+                //});
+
+                //return task;
+
+                try
+                {
+                    // Read the form data.
+                    await Request.Content.ReadAsMultipartAsync(streamProvider);
+
+                    //var dbUser = unitOfWork.Users.All().FirstOrDefault(x => x.Sessionkey == sessionkey);
+                    var uploader = new DropboxUploader();
+                    List<string> urls = new List<string>();
+                    foreach (MultipartFileData file in streamProvider.FileData)
+                    {
+                        //Trace.WriteLine(file.Headers.ContentDisposition.FileName);
+                        //Trace.WriteLine("Server file path: " + file.LocalFileName);
+                        string fileName = file.LocalFileName;
+                        var url = uploader.UploadFileToDropBox(fileName, file.Headers.ContentDisposition.FileName);
+                        urls.Add(url.ToString());
+                        //dbUser.ProfilePicture = url;
+
+                        //break;
+                    }
+                    return Request.CreateResponse(HttpStatusCode.OK, urls);
+                }
+                catch (System.Exception e)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
+                }
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotAcceptable, "This request is not properly formatted"));
+            }
+
+        }
+    }
+}
---
+++ TeamTimon/Chat.Services/Utils/DropboxUploader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Spring.IO;
+using Spring.Social.Dropbox.Api;
+using Spring.Social.Dropbox.Connect;
+using Spring.Social.OAuth1;
+
+namespace Chat.Services.Utils
+{
+    public class DropboxUploader
+    {
+        private const string DropboxAppKey = "u45v5vze1tqrko8";
+        private const string DropboxAppSecret = "01g02ukz96pyldu";
+
+        public string UploadFileToDropBox(string filePath, string fileName)
+        {
+            DropboxServiceProvider dropboxServiceProvider =
+                           new DropboxServiceProvider(DropboxAppKey, DropboxAppSecret, AccessLevel.AppFolder);
+
+            // Authenticate the application (if not authenticated) and load the OAuth token
+            // TODO - throw exception if auth file is not found
+            //if (!File.Exists(OAuthTokenFileName))
+            //{
+            //    AuthorizeAppOAuth(dropboxServiceProvider);
+            //}
+            OAuthToken oauthAccessToken = LoadOAuthToken();
+
+            // Login in Dropbox
+            IDropbox dropbox = dropboxServiceProvider.GetApi(oauthAccessToken.Value, oauthAccessToken.Secret);
+
+            // Display user name (from his profile)
+            //DropboxProfile profile = dropbox.GetUserProfileAsync().Result;
+            //Console.WriteLine("Hi " + profile.DisplayName + "!");
+
+            // Create new folder
+            string newFolderName = DateTime.Now.Ticks.ToString();
+            //Entry createFolderEntry = dropbox.CreateFolderAsync(newFolderName).Result;
+            Entry createFolderEntry = dropbox.CreateFolder(newFolderName);
+            // Upload a file
+            Entry uploadFileEntry = dropbox.UploadFile(
+                new FileResource(filePath),
+                "/" + newFolderName + "/" + fileName);
+
+            // Share a file
+            //DropboxLink sharedUrl = dropbox.GetShareableLinkAsync(uploadFileEntry.Path).Result;
+            DropboxLink sharedUrl = dropbox.GetShareableLink(uploadFileEntry.Path);
+            var ret = sharedUrl.Url.ToString();
+            return ret;
+        }
+
+        private OAuthToken LoadOAuthToken()
+        {
+            OAuthToken oauthAccessToken = new OAuthToken("hsmlwuul3nbd72go", "z183wdbxk9dj49i");
+            return oauthAccessToken;
+        }
+
+        //private void AuthorizeAppOAuth(DropboxServiceProvider dropboxServiceProvider)
+        //{
+        //    // Authorization without callback url
+        //    OAuthToken oauthToken = dropboxServiceProvider.OAuthOperations.FetchRequestTokenAsync(null, null).Result;
+        //    OAuth1Parameters parameters = new OAuth1Parameters();
+        //    string authenticateUrl = dropboxServiceProvider.OAuthOperations.BuildAuthorizeUrl(
+        //        oauthToken.Value, parameters);
+        //    AuthorizedRequestToken requestToken = new AuthorizedRequestToken(oauthToken, null);
+        //    OAuthToken oauthAccessToken =
+        //        dropboxServiceProvider.OAuthOperations.ExchangeForAccessTokenAsync(requestToken, null).Result;
+        //    string[] oauthData = new string[] { oauthAccessToken.Value, oauthAccessToken.Secret };
+        //    File.WriteAllLines(OAuthTokenFileName, oauthData);
+        //}
+    }
+}
