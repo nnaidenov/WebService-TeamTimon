fb5b7448cc10bc8d7f74306002da8b97d3a52fd0
Daniel <Drgreen_bg@yahoo.com>
2013-08-15 12:39:22 +0300

    Added MessageModel

---
+++ TeamTimon/Chat.Services/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat.Services.Models
+{
+    public class MessageModel
+    {
+        public int UserID { get; set; }
+        public int ChatID { get; set; }
+        public string Content { get; set; }
+    }
+}
