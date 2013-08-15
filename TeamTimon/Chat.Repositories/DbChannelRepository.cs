38320d25978c5b2e6c240332eda208e5a912abfb
nnaidenov <n.naidenov92@gmail.com>
2013-08-15 12:54:10 +0300

    Create ChannelRepository and ChannelController

---
+++ TeamTimon/Chat.Repositories/DbChannelRepository.cs
@@ -0,0 +1,44 @@
+﻿using Chat.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Chat.Repositories
+{
+    class DbChannelRepository : IRepository<Channel>
+    {
+        public Channel Add(Channel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Channel Update(int id, Channel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Channel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Channel Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Channel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
